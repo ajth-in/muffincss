d86383e4701cf3725e464794729d9170a0273b9b
Ajith Kumar P M <ajith@strollby.com>
2025-05-06 23:50:06 +0530

    fix: update name to muffincss

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["@anucss/eslint-config/next.js"],
+  extends: ["@muffincss/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
