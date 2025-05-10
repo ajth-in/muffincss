285f7074955b702f72f6702cccbef3e70221ffef
ajth-in <ajithpmuralidharan01@gmail.com>
2025-05-10 12:50:10 +0530

    feat(postcss): add processors for media rules

--- packages/muffincss-postcss/src/types.ts
+++ packages/muffincss-postcss/src/types.ts
@@ -1,19 +1,44 @@
-export type PluginOptions = {
+/**
+ * Configuration options for the CSS atomizer.
+ */
+export type AtomizerOptions = {
   /**
    * If true, compiles all CSS styles into atomic utility classes.
+   * @default true
    */
   optimize?: boolean;
 
   /**
    * If true, removes all unused CSS styles during the build process.
+   * @default true
    */
   purge?: boolean;
 
   /**
    * Specifies the CSS reset strategy to apply.
    * - "minimal": Applies a minimal reset.
-   * - "default": Applies the default reset (default value).
+   * - "default": Applies the default reset.
    * - "off": Disables CSS reset entirely.
+   * @default "default"
    */
   reset?: "minimal" | "default" | "off";
+
+  /**
+   * Prefix to apply to all generated utility class names.
+   * @default "a-"
+   */
+  prefix?: string;
+
+  /**
+   * List of CSS selectors and properties to exclude from processing.
+   */
+  exclude?: {
+    selectors: RegExp[];
+    properties: RegExp[];
+  };
 };
+
+export interface AtomicRule {
+  prop: string;
+  value: string;
+}
---
+++ packages/muffincss-postcss/src/utils.ts
@@ -0,0 +1,32 @@
+import { AtRule, Declaration, Rule } from "postcss";
+import type { AtomicRule } from "./types";
+
+export const formatToId = (input: string) =>
+  input.replace(/[^a-zA-Z0-9]/g, "_");
+
+export const stringifyDeclaration = (
+  decl: Declaration,
+  prefix: string,
+  mediaQueryParam?: string,
+) => {
+  const declarationId = `${prefix}${decl.prop}-${formatToId(decl.value)}`;
+  return mediaQueryParam
+    ? `${declarationId}-${formatToId(mediaQueryParam)}`
+    : declarationId;
+};
+
+export const generateMediaRules = (
+  rules: Map<string, AtomicRule>,
+  mediaQuery: string,
+) => {
+  const mediaRule = new AtRule({ name: "media", params: mediaQuery });
+
+  rules.forEach((data, className) => {
+    let selector = `.${className}`;
+
+    const newRule = new Rule({ selector });
+    newRule.append(new Declaration({ prop: data.prop, value: data.value }));
+    mediaRule.append(newRule);
+  });
+  return mediaRule;
+};
