8931907006ad0c901dd0241a9e313fbf336c29ab
Ajith Kumar P M <ajith@strollby.com>
2025-05-26 00:39:55 +0530

    feat(css): update output directory and import paths for MuffinCSS styles

--- apps/docs/src/components/common/ThemeSwitch/index.tsx
+++ apps/docs/src/components/common/ThemeSwitch/index.tsx
@@ -1,7 +1,7 @@
 import { MoonStar, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Button } from "react-aria-components";
-import css from "~/muffin/css";
+import css from "~/_muffin/css";
 
 export default function ThemeSwitch() {
   const [darkMode, setDarkMode] = useState<boolean | null>(null);
