4060f29a9733485dbfd195aee000e399babe38f3
Ajith Kumar P M <ajith@strollby.com>
2025-05-25 23:07:16 +0530

    refactor(CodeViewer): streamline tab data structure and enhance code rendering

--- apps/docs/src/components/common/hero/CodeViewer/index.tsx
+++ apps/docs/src/components/common/hero/CodeViewer/index.tsx
@@ -1,72 +1,59 @@
 import { Tabs, TabList, Tab, TabPanel } from "react-aria-components";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { a11yDark as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { hopscotch as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
 import css from "~/muffin/css";
 import { input, output } from "./utils";
-const codeString = `function greet(name) {
-    return 'Hello, ' + name;
-}
-`;
-const tabs = [
-  { id: "FoR", label: "input" },
-  { id: "MaR", label: "output" },
+
+const tabData = [
+  { id: "FoR", label: "input", content: input },
+  { id: "MaR", label: "output", content: output },
 ];
 
 const getTabClass = ({ isSelected }: { isSelected: boolean }) =>
+  // todo: bug in muffin to detect data classes
   css(
-    isSelected
-      ? ["react-aria-Tab", "react-aria-Tab-selected"]
-      : ["react-aria-Tab"],
+    ["react-aria-Tab", isSelected && "react-aria-Tab-selected"].filter(
+      (item): item is string => Boolean(item),
+    ),
   );
+
+const renderCodeBlocks = (code: { css: string; js: string }) => (
+  <>
+    <SyntaxHighlighter
+      language="css"
+      style={theme}
+      showLineNumbers
+      customStyle={{ fontSize: "12px" }}
+    >
+      {code.css}
+    </SyntaxHighlighter>
+    <SyntaxHighlighter
+      language="javascript"
+      style={theme}
+      showLineNumbers
+      customStyle={{ fontSize: "12px" }}
+    >
+      {code.js}
+    </SyntaxHighlighter>
+  </>
+);
 
 const CodeViewer = () => {
   return (
     <Tabs className={css(["react-aria-Tabs"])}>
       <TabList className={css(["react-aria-TabList"])} aria-label="Code Viewer">
-        {tabs.map(({ id, label }) => (
+        {tabData.map(({ id, label }) => (
           <Tab key={id} id={id} className={getTabClass}>
             {label}
           </Tab>
         ))}
       </TabList>
 
-      <TabPanel className={css(["react-aria-TabPanel"])} id="FoR">
-        <SyntaxHighlighter
-          language="css"
-          style={theme}
-          showLineNumbers
-          customStyle={{ fontSize: "12px" }}
-        >
-          {input.css}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter
-          language="javascript"
-          style={theme}
-          showLineNumbers
-          customStyle={{ fontSize: "12px" }}
-        >
-          {input.js}
-        </SyntaxHighlighter>
-      </TabPanel>
-
-      <TabPanel className={css(["react-aria-TabPanel"])} id="MaR">
-        <SyntaxHighlighter
-          language="css"
-          style={theme}
-          showLineNumbers
-          customStyle={{ fontSize: "12px" }}
-        >
-          {output.css}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter
-          language="javascript"
-          style={theme}
-          showLineNumbers
-          customStyle={{ fontSize: "12px" }}
-        >
-          {output.js}
-        </SyntaxHighlighter>
-      </TabPanel>
+      {tabData.map(({ id, content }) => (
+        <TabPanel key={id} id={id} className={css(["react-aria-TabPanel"])}>
+          {renderCodeBlocks(content)}
+        </TabPanel>
+      ))}
     </Tabs>
   );
 };
