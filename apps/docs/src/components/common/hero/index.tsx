bc497c20aaae0886625334d92004fd1a4d75086e
Ajith Kumar P M <ajithpmuralidharan01@gmail.com>
2025-05-26 00:33:59 +0530

    Feat/tanstack docs (#12)
    * feat: update package name to @muffincss/css
    
    * fix(docs): add react aria components
    
    * feat(postcss):  postcss configuration and add exclude options for selectors
    
    * feat: add ThemeSwitch component and update styles; integrate Header with navigation links
    
    * fix(docs): remove existing doc files
    
    * feat(docs): add header
    
    * feat(docs): add hero section
    
    * feat(docs): add code view section
    
    * refactor(CodeViewer): streamline tab data structure and enhance code rendering
    
    * feat(Features): add FeaturesSection component with feature cards and styles
    refactor(Hero): update action links and improve layout responsiveness
    refactor(CodeViewer): remove line numbers from code blocks for cleaner display
    style(app): enhance global styles with new breakpoints and surface colors
    
    * feat(docs): add footer component with styling
    
    * feat(docs): update site manifest and root route metadata for MuffinCSS
    
    * feat(config): configure server preset for Vercel
    
    * fix: remove obsolete icon files and add new MuffinCSS logo SVG
    
    ---------

---
+++ apps/docs/src/components/common/hero/index.tsx
@@ -0,0 +1,46 @@
+import { Link } from "@tanstack/react-router";
+import css from "~/muffin/css";
+import CodeViewer from "./CodeViewer";
+const actions = [
+  {
+    href: "/",
+    text: "Get Started",
+    variant: "primary",
+  },
+  {
+    href: "/",
+    text: "Installation",
+    variant: "secondary",
+  },
+];
+
+export default function Hero() {
+  return (
+    <section className={css(["hero"])}>
+      <div className={css(["hero-content"])}>
+        <h1 className={css(["hero-title"])}>
+          <span className={css(["hero-brand"])}>🧁 Muffin CSS</span>
+          <br />
+          CSS pre-processor
+        </h1>
+        <p className={css(["hero-subtitle"])}>
+          Transform plain CSS into atomic, type-safe utilities{" "}
+        </p>
+        <div className={css(["hero-actions"])}>
+          {actions.map((action) => (
+            <Link
+              className={css(["button", `button--${action.variant}`])}
+              key={action.text}
+              to={action.href}
+            >
+              {action.text}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className={css(["hero-codeblock"])}>
+        <CodeViewer />
+      </div>
+    </section>
+  );
+}
