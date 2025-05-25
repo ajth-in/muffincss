fc25d2f73673d67edfa4c134149f1ff0309e931f
Ajith Kumar P M <ajith@strollby.com>
2025-05-25 21:16:43 +0530

    feat(docs): add hero section

--- apps/docs/src/components/common/hero/index.tsx
+++ apps/docs/src/components/common/hero/index.tsx
@@ -1,30 +1,40 @@
+import { Link } from "@tanstack/react-router";
 import css from "~/muffin/css";
+const actions = [
+  {
+    href: "/getting-started",
+    text: "Get Started",
+    variant: "primary",
+  },
+  {
+    href: "/installation",
+    text: "Installation",
+    variant: "secondary",
+  },
+];
 
 export default function Hero() {
   return (
     <section className={css(["hero"])}>
       <div className={css(["hero-content"])}>
         <h1 className={css(["hero-title"])}>
-          <span className={css(["hero-brand"])}>Shiki 式</span>
+          <span className={css(["hero-brand"])}>🧁 Muffin CSS</span>
           <br />
-          Syntax highlighter
+          CSS pre-processor
         </h1>
         <p className={css(["hero-subtitle"])}>
-          A beautiful yet powerful syntax highlighter
+          Transform plain CSS into atomic, type-safe utilities{" "}
         </p>
         <div className={css(["hero-actions"])}>
-          <a
-            href="/getting-started"
-            className={css(["button", "button--primary"])}
-          >
-            Get Started
-          </a>
-          <a
-            href="/installation"
-            className={css(["button", "button--secondary"])}
-          >
-            Installation
-          </a>
+          {actions.map((action) => (
+            <Link
+              className={css(["button", `button--${action.variant}`])}
+              key={action.text}
+              to={action.href}
+            >
+              {action.text}
+            </Link>
+          ))}
         </div>
       </div>
       <div className={css(["hero-codeblock"])}>hi</div>
