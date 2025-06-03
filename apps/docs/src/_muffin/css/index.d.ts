8f582cb1446b9e0a85dc048238786ccaa1cff5bc
Ajith Kumar P M <ajith@strollby.com>
2025-06-04 00:20:33 +0530

    refactor: format type declarations for improved readability

--- apps/docs/src/_muffin/css/index.d.ts
+++ apps/docs/src/_muffin/css/index.d.ts
@@ -1,3 +1,5 @@
 import classMap from "../_resolved";
-declare function css(classNames: (keyof typeof classMap | (string & {}))[]): string;
+declare function css(
+  classNames: (keyof typeof classMap | (string & {}))[],
+): string;
 export default css;
