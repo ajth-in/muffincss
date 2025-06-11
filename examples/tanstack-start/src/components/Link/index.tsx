48c6e63756bf6448b61c4203ab2c2ae7b14e9c8d
Ajith Kumar P M <ajith@strollby.com>
2025-06-11 21:46:21 +0530

    refactor: remove tanstack example

--- examples/tanstack-start/src/components/Link/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Link as TanStackLink } from "@tanstack/react-router";
-import { ComponentProps } from "react";
-const Link = (props: ComponentProps<typeof TanStackLink>) => {
-  const { children, ...linkProps } = props;
-  return <TanStackLink {...linkProps}>{children}</TanStackLink>;
-};
-
-export default Link;
