313eb6061e48107381d851058253e35ffed86b8c
ajth-in <ajithpmuralidharan01@gmail.com>
2025-06-14 11:03:20 +0530

    fix: remove obsolete muffin CSS and TypeScript files from the documentation

--- apps/docs/src/_muffin/css/index.mjs
+++
@@ -1,6 +0,0 @@
-import classMap from "../_resolved";
-import {mergeResolved} from "@muffincss/postcss/tools";
-function css(classNames) {
-  return mergeResolved(classNames,classMap)
-}
-export default css;
