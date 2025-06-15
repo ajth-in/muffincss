249df393fa16945df6d3d5c66de0b5f8f41e2b11
ajth-in <ajithpmuralidharan01@gmail.com>
2025-06-15 20:25:08 +0530

    feat: implement utility class name construction with abbreviations for properties and pseudo-classes

--- packages/muffincss-core/src/utils/construct-utility-name.ts
+++ packages/muffincss-core/src/utils/construct-utility-name.ts
@@ -2,8 +2,37 @@ import type { AtRule, Declaration } from "postcss";
 import type { SelectorComponents } from "./parse-selector";
 import type { MuffinConfig } from "../types";
 import { x86 } from "murmurhash3js";
+import { abbreviations, pseudoAbbreviations } from "./abbr";
 
-const sanitize = (str: string) => str.replace(/[^a-zA-Z0-9]/g, "_");
+const sanitize = (str: string) => str.replace(/[^a-zA-Z0-9\-]/g, "_");
+
+const stringifyParentAtRule = (parentAtRule?: AtRule) => {
+  if (!parentAtRule) return;
+  const atRuleAbrev = (() => {
+    if (parentAtRule.name === "media") return "m";
+    if (parentAtRule.name === "container") return "c";
+    return parentAtRule.name;
+  })();
+  return `${atRuleAbrev}-${parentAtRule.params.replace(/[^a-zA-Z0-9]/g, "")}_`;
+};
+
+const stringifyPsedo = ({
+  pseudoClasses,
+  pseudoElement,
+}: SelectorComponents) => {
+  const selector = pseudoClasses
+    ? (pseudoAbbreviations[pseudoClasses] ?? pseudoClasses)
+    : "";
+  const element = pseudoElement
+    ? (pseudoAbbreviations[pseudoElement] ?? pseudoElement)
+    : "";
+  return `-${selector}${element ? `-${element}` : ""}`;
+};
+const stringifyDeclaration = (declaration: Declaration) => {
+  const prop = abbreviations[declaration.prop] ?? declaration.prop;
+  const value = abbreviations[declaration.value] ?? declaration.value;
+  return `${prop}_${value}`;
+};
 
 const constructUtilityClassName = (
   declaration: Declaration,
@@ -14,11 +43,9 @@ const constructUtilityClassName = (
   const { parentAtRule } = context ?? {};
 
   const rawParts = [
-    declaration.prop,
-    declaration.value,
-    selector.pseudoClasses,
-    parentAtRule?.name,
-    parentAtRule?.params,
+    stringifyParentAtRule(parentAtRule),
+    stringifyDeclaration(declaration),
+    stringifyPsedo(selector),
   ];
 
   const sanitizedName = rawParts
